9920646d9d077882a81d0eae1d81c6a5e12741c9
Andrew Stangl <andrew.stangl@tesco.com>
2026-03-25 12:25:34 +0200

    More mocks

--- packages/k8s/jest.setup.js
+++ packages/k8s/jest.setup.js
@@ -5,13 +5,13 @@ jest.setTimeout(500000)
 jest.mock('@kubernetes/client-node', () => {
 	const actual = jest.requireActual('@kubernetes/client-node');
 	class MockCoreV1Api {
-		createNamespacedPod = jest.fn().mockResolvedValue({ body: {} });
-		deleteNamespacedPod = jest.fn().mockResolvedValue({ body: {} });
-		listNamespacedPod = jest.fn().mockResolvedValue({ body: { items: [] } });
-		listNamespacedSecret = jest.fn().mockResolvedValue({ body: { items: [] } });
-		deleteNamespacedSecret = jest.fn().mockResolvedValue({ body: {} });
-		readNamespacedPod = jest.fn().mockResolvedValue({ body: {} });
-		patchNamespacedPod = jest.fn().mockResolvedValue({ body: {} });
+		createNamespacedPod = jest.fn().mockResolvedValue({});
+		deleteNamespacedPod = jest.fn().mockResolvedValue({});
+		listNamespacedPod = jest.fn().mockResolvedValue({ items: [] });
+		listNamespacedSecret = jest.fn().mockResolvedValue({ items: [] });
+		deleteNamespacedSecret = jest.fn().mockResolvedValue({});
+		readNamespacedPod = jest.fn().mockResolvedValue({});
+		patchNamespacedPod = jest.fn().mockResolvedValue({});
 		// Add more mocked methods as needed for your tests
 	}
 	return {
