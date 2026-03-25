2b321f50b2a59420f74a313008b53b5c55e3d34a
Andrew Stangl <andrew.stangl@tesco.com>
2026-03-25 11:43:18 +0200

    Fix tests?

--- packages/k8s/tests/test-setup.ts
+++ packages/k8s/tests/test-setup.ts
@@ -4,10 +4,16 @@ import { HookData } from 'hooklib/lib'
 import * as path from 'path'
 import { v4 as uuidv4 } from 'uuid'
 
-const kc = new k8s.KubeConfig()
-
 kc.loadFromDefault()
 
+const kc = new k8s.KubeConfig()
+kc.loadFromDefault()
+// Allow HTTP clusters in test by setting skipTLSVerify=true
+kc.clusters.forEach(cluster => {
+  if (cluster.server && cluster.server.startsWith('http://')) {
+    cluster.skipTLSVerify = true
+  }
+})
 const k8sApi = kc.makeApiClient(k8s.CoreV1Api)
 
 export class TestHelper {
