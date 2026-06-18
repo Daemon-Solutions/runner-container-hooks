7bfd6c4bcb728eeaf8f4a0888bbb28d247d32105
Andrew Stangl <andrew.stangl@tesco.com>
2026-06-18 17:09:29 +0200

    Better handling for openshift use case

--- packages/k8s/src/k8s/index.ts
+++ packages/k8s/src/k8s/index.ts
@@ -505,7 +505,7 @@ export async function execCpToPod(
           `mkdir -p "$tmp_dir"; ` +
           `ls -ld ${shlex.quote(containerPath)} "$tmp_dir"; ` +
           `tar xf - -m -o --no-same-permissions --exclude='.' -C "$tmp_dir"; ` +
-          `find "$tmp_dir" -mindepth 1 -maxdepth 1 -exec mv {} ${shlex.quote(containerPath)}/ \;; ` +
+          `find "$tmp_dir" -mindepth 1 -maxdepth 1 -exec mv {} ${shlex.quote(containerPath)}/ ; ` +
           `rmdir "$tmp_dir"; ` +
           `find ${shlex.quote(containerPath)} -type f -exec chmod u+rw {} \\;; ` +
           `find ${shlex.quote(containerPath)} -type d -exec chmod u+rwx {} \\;; ` +
