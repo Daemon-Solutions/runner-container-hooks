eb690fb7cd23248f7d133892e7ec98d5c8eb31ad
Andrew Stangl <andrew.stangl@tesco.com>
2026-06-18 17:05:35 +0200

    Better handling for openshift use case

--- packages/k8s/src/k8s/index.ts
+++ packages/k8s/src/k8s/index.ts
@@ -499,9 +499,14 @@ export async function execCpToPod(
         'sh',
         '-c',
         `set -eux; ` +
+          `tmp_dir=${shlex.quote(path.posix.join(containerPath, '.runner-tmp-copy'))}; ` +
           `mkdir -p ${shlex.quote(containerPath)}; ` +
-          `ls -ld ${shlex.quote(containerPath)}; ` +
-          `tar xf - -m -o --no-same-permissions --exclude='.' -C ${shlex.quote(containerPath)}; ` +
+          `rm -rf "$tmp_dir"; ` +
+          `mkdir -p "$tmp_dir"; ` +
+          `ls -ld ${shlex.quote(containerPath)} "$tmp_dir"; ` +
+          `tar xf - -m -o --no-same-permissions --exclude='.' -C "$tmp_dir"; ` +
+          `find "$tmp_dir" -mindepth 1 -maxdepth 1 -exec mv {} ${shlex.quote(containerPath)}/ \;; ` +
+          `rmdir "$tmp_dir"; ` +
           `find ${shlex.quote(containerPath)} -type f -exec chmod u+rw {} \\;; ` +
           `find ${shlex.quote(containerPath)} -type d -exec chmod u+rwx {} \\;; ` +
           `echo '__RUNNER_CP_TO_POD_DONE__'`
